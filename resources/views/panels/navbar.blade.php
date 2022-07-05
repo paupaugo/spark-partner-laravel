1b54d459ceda73e81cdb05b44f23fffda5e8987d
johnpaulocruz <johnpaulocruz@smedia.com.au>
2022-07-05 23:12:57 +0800

    remove fix

--- resources/views/panels/navbar.blade.php
+++ resources/views/panels/navbar.blade.php
@@ -109,7 +109,7 @@
         <li class="nav-item dropdown dropdown-user">
           <a class="nav-link dropdown-toggle dropdown-user-link" id="dropdown-user" href="javascript:void(0);" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
             <div class="user-nav d-sm-flex d-none">
-              <span class="user-name font-weight-bolder">{{
+              <span class="user-name font-weight-bolder">
               
               
               sPARKY </span>
